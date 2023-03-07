6dc1969b271b0e0e7b9020f0696bcc2344c84018
huappe <155170805+huappe@users.noreply.github.com>
2023-03-07 12:57:33 +0000

    erase

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -158,4 +158,22 @@ class WandbLogger():
 
     def download_dataset_artifact(self, path, alias):
         if isinstance(path, str) and path.startswith(WANDB_ARTIFACT_PREFIX):
-            dataset_artifact = wandb
+            dataset_artifact = wandb.use_artifact(remove_prefix(path, WANDB_ARTIFACT_PREFIX) + ":" + alias)
+            assert dataset_artifact is not None, "'Error: W&B dataset artifact doesn\'t exist'"
+            datadir = dataset_artifact.download()
+            return datadir, dataset_artifact
+        return None, None
+
+    def download_model_artifact(self, opt):
+        if opt.resume.startswith(WANDB_ARTIFACT_PREFIX):
+            model_artifact = wandb.use_artifact(remove_prefix(opt.resume, WANDB_ARTIFACT_PREFIX) + ":latest")
+            assert model_artifact is not None, 'Error: W&B model artifact doesn\'t exist'
+            modeldir = model_artifact.download()
+            epochs_trained = model_artifact.metadata.get('epochs_trained')
+            total_epochs = model_artifact.metadata.get('total_epochs')
+            assert epochs_trained < total_epochs, 'training to %g epochs is finished, nothing to resume.' % (
+                total_epochs)
+            return modeldir, model_artifact
+        return None, None
+
+    de
