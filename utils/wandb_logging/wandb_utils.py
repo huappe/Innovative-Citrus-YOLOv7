2ef7e0a87935c926c367fde4150276a80c8e17e5
huappe <155170805+huappe@users.noreply.github.com>
2023-03-07 13:12:54 +0000

    MIT License

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -176,4 +176,22 @@ class WandbLogger():
             return modeldir, model_artifact
         return None, None
 
-    de
+    def log_model(self, path, opt, epoch, fitness_score, best_model=False):
+        model_artifact = wandb.Artifact('run_' + wandb.run.id + '_model', type='model', metadata={
+            'original_url': str(path),
+            'epochs_trained': epoch + 1,
+            'save period': opt.save_period,
+            'project': opt.project,
+            'total_epochs': opt.epochs,
+            'fitness_score': fitness_score
+        })
+        model_artifact.add_file(str(path / 'last.pt'), name='last.pt')
+        wandb.log_artifact(model_artifact,
+                           aliases=['latest', 'epoch ' + str(self.current_epoch), 'best' if best_model else ''])
+        print("Saving model artifact on epoch ", epoch + 1)
+
+    def log_dataset_artifact(self, data_file, single_cls, project, overwrite_config=False):
+        with open(data_file) as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)  # data dict
+        nc, names = (1, ['item']) if single_cls else (int(data['nc']), data['names'])
+        names = {k: v for k, v 
